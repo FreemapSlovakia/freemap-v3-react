aaee8f8ccdd4cd97dd2489af727fb91eba324180
Peter Vojtek <peter.vojtek@gmail.com>
2017-02-24 16:56:20 +0100

    code cleanup

--- src/app.js
+++ src/app.js
@@ -1,8 +1,6 @@
 import Main from './main.jsx';
 import React from 'react';
 import ReactDOM from 'react-dom';
-var Locations = Router.Locations
-var Location = Router.Location
 import { hashHistory, Router, Route } from 'react-router'
 
 ReactDOM.render((
--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,8 @@
 import React from 'react';
 import { Map, TileLayer, Marker, LayersControl } from 'react-leaflet';
-import FileSaver from 'file-saver';
 import Navbar from 'react-bootstrap/lib/Navbar';
-import Nav from 'react-bootstrap/lib/Nav';
-import NavItem from 'react-bootstrap/lib/NavItem';
-import FormGroup from 'react-bootstrap/lib/FormGroup';
-import ControlLabel from 'react-bootstrap/lib/ControlLabel';
 import Form from 'react-bootstrap/lib/Form';
 import FormControl from 'react-bootstrap/lib/FormControl';
-import NavDropdown from 'react-bootstrap/lib/NavDropdown';
-import MenuItem from 'react-bootstrap/lib/MenuItem';
-import Checkbox from 'react-bootstrap/lib/Checkbox';
-import Glyphicon from 'react-bootstrap/lib/Glyphicon';
-import Panel from 'react-bootstrap/lib/Panel';
-import ButtonGroup from 'react-bootstrap/lib/ButtonGroup';
 import Button from 'react-bootstrap/lib/Button';
 import { hashHistory } from 'react-router'
 import mapDefinitions from './mapDefinitions';
@@ -26,10 +15,10 @@ export default class Main extends React.Component {
   constructor(props) {
     super(props);
 
-    var mapType = props.params.mapType || 'T'
-    var zoom = parseInt(props.params.zoom) || 8
-    var lat = props.params.lat || 48.70714
-    var lon = props.params.lon || 19.4995
+    const mapType = props.params.mapType || 'T'
+    const zoom = parseInt(props.params.zoom) || 8
+    const lat = props.params.lat || 48.70714
+    const lon = props.params.lon || 19.4995
 
     this.state = Object.assign({}, cleanState, {
       map: mapType, // TODO: rename attribute name from map to mapType!!
@@ -42,15 +31,14 @@ export default class Main extends React.Component {
   }
 
   componentDidUpdate() {
-    var zoom = this.state.zoom
-    var lat = this.state.center.lat.toFixed(5)
-    var lon = this.state.center.lng.toFixed(5)
-    var mapType = this.state.map
-    var p = this.props.params
-    if(p.lat != lat || p.lon != lon || p.zoom != zoom || p.mapType != mapType)
-      hashHistory.push('/'+mapType+'/'+zoom+'/'+lat+'/'+lon)
+    const zoom = this.state.zoom
+    const lat = this.state.center.lat.toFixed(5)
+    const lon = this.state.center.lng.toFixed(5)
+    const mapType = this.state.map
+    const p = this.props.params
+    if(p.lat !== lat || p.lon !== lon || p.zoom !== zoom || p.mapType !== mapType)
+      hashHistory.push(`/${mapType}/${zoom}/${lat}/${lon}`)
   }
-
 
   handleMapMoveend(e) {
     this.setState({ center: e.target.getCenter() });
@@ -69,20 +57,18 @@ export default class Main extends React.Component {
   }
 
   doSearch(){
-    var q = this.state['searchQuery']
-    var lat = this.state['lat']
-    var lon = this.state['lon']
-    var url = 'http://www.freemap.sk/api/0.1/q/'+q+'&lat='+lat+'&lon='+lat
-    var that = this
+    const q = this.state['searchQuery']
+    const lat = this.state['lat']
+    const lon = this.state['lon']
+    const url = 'http://www.freemap.sk/api/0.1/q/'+q+'&lat='+lat+'&lon='+lon
     fetch(url, {
         method: 'GET'
       }).then(res => res.json()).then(data => {
-        var counter = 1
-        var results = data.map((d, id) => {
-          return {id: id, lat : d.lat, lon: d.lon, name: d.name}
+        const results = data.map((d, id) => {
+          return {id, lat : d.lat, lon: d.lon, name: d.name}
         })
         if(results.length > 0){
-          var firstResult = results[0]
+          const firstResult = results[0]
           this.setState({ searchResults: results, center: L.latLng(firstResult.lat, firstResult.lon) , zoom: 14  });
         } else {
           this.setState({ searchResults: results });
