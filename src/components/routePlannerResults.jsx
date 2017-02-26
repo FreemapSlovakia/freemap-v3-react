16679465a34c4e498afcda19abd49c206b49bbbe
Peter Vojtek <peter.vojtek@gmail.com>
2017-02-26 18:23:50 +0100

    #4: minor

--- src/components/routePlannerResults.jsx
+++ src/components/routePlannerResults.jsx
@@ -18,26 +18,30 @@ export default class RoutePlannerResults extends React.Component {
     // FIXME: there must be some nicer way to do this
     const changed = JSON.stringify(p) != JSON.stringify(this.state.routePlannerPoints)
     if(changed){
-      this.setState({ routePlannerPoints: p });
+      this.setState({ routePlannerPoints: p, routeShapePoints: [] })
+      if(p.start.lat && p.finish.lat)
+        this.computeNewRoute()
     }
+  }
+
+  computeNewRoute(){
     const that = this
-    if(changed && p.start.lat && p.finish.lat){
-      const url = `http://www.freemap.sk/api/0.1/r/${p.start.lat}%7C${p.start.lon}/${p.finish.lat}%7C${p.finish.lon}/motorcar/fastest&Ajax=`
-      fetch(url, {
-        method: 'GET'
-      }).then(res => res.text()).then(data => {
-        xml2js(data, (error, json) => {
-          const rawPointsWithMess = json.osmRoute.wkt[0]
-          const rawPoints =  rawPointsWithMess.substring(14, rawPointsWithMess.length - 3)
-          const points = rawPoints.split(', ').map((lonlat) => {
-            const lonlatArray = lonlat.split(' ')
-            return [parseFloat(lonlatArray[1]), parseFloat(lonlatArray[0])]
-          })
-          that.setState({ routeShapePoints: points});
+
+    const url = `http://www.freemap.sk/api/0.1/r/${p.start.lat}%7C${p.start.lon}/${p.finish.lat}%7C${p.finish.lon}/motorcar/fastest&Ajax=`
+    fetch(url, {
+      method: 'GET'
+    }).then(res => res.text()).then(data => {
+      xml2js(data, (error, json) => {
+        const rawPointsWithMess = json.osmRoute.wkt[0]
+        const rawPoints =  rawPointsWithMess.substring(14, rawPointsWithMess.length - 3)
+        const points = rawPoints.split(', ').map((lonlat) => {
+          const lonlatArray = lonlat.split(' ')
+          return [parseFloat(lonlatArray[1]), parseFloat(lonlatArray[0])]
         })
 
-      });
-    }
+        that.setState({ routeShapePoints: points})
+      })
+    });
   }
 
   render() {
