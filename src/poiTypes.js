42ede2e1152c4a8d891bd519d38e531faa4529d4
Martin Ždila <m.zdila@gmail.com>
2017-02-26 13:36:11 +0100

    code cleanup

--- src/poiTypes.js
+++ src/poiTypes.js
@@ -7,24 +7,20 @@ const nf = Intl.NumberFormat('sk', { minimumFractionDigits: 0, maximumFractionDi
 
 export const poiTypes = [
   { title: 'Vrchol', key: 'natural', value: 'peak', group: 'nature',
-    template({ name, ele }) {
-      return `Vrchol${name ? `<br/>${escapeHtml(name)}` : ''}${ele ? `${name ? ' ' : '<br/>'} (${nf.format(ele)} m)` : '' }`;
-    }
+    template: ({ name, ele }) => `Vrchol${name ? `<br/>${escapeHtml(name)}` : ''}${ele ? `${name ? ' ' : '<br/>'} (${nf.format(ele)} m)` : '' }`
   },
   { title: 'Prameň', key: 'natural', value: 'spring', group: 'nature',
-    template({ name }) {
-      return `Prameň${name ? `<br/>${escapeHtml(name)}` : ''}`;
-    }
+    template: ({ name }) => `Prameň${name ? `<br/>${escapeHtml(name)}` : ''}`
   },
   { title: 'Potraviny', key: 'shop', value: 'convenience', group: 'shop',
-    template({ name }) {
-      return `Potraviny${name ? `<br/>${escapeHtml(name)}` : ''}`;
-    }
+    template: ({ name }) => `Potraviny${name ? `<br/>${escapeHtml(name)}` : ''}`
   }
 ];
 
+const keys = new Set(poiTypes.map(pt => pt.key));
+
 export function toHtml(tags) {
-  for (let key of [ 'natural', 'shop' ]) {
+  for (let key of keys) {
     if (tags[key]) {
       const pt = poiTypes.find(pt => pt.key === key && pt.value === tags[key]);
       if (pt) {
@@ -32,7 +28,7 @@ export function toHtml(tags) {
       }
     }
   }
-console.log("TT", tags);
+
   return tags.name && escapeHtml(tags.name);
 }
 
