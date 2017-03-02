3cbe80c954cdd113bdeb5346becf6f36bdb0d6d0
Martin Ždila <martin@gruveo.com>
2017-03-02 10:55:20 +0100

    workaround for source map generation

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,9 @@ module.exports = {
     filename: 'dist/app.js',
     path: __dirname
   },
-  devtool: process.env.NODE_ENV === 'production' ? 'source-map' : 'cheap-module-eval-source-map',
+  // more info:https://webpack.github.io/docs/build-performance.html#sourcemaps and https://webpack.github.io/docs/configuration.html#devtool
+  // #cheap-module-eval-source-map doesn't work - see https://github.com/webpack/webpack/issues/2145
+  devtool: process.env.NODE_ENV === 'production' ? 'source-map' : 'cheap-module-source-map',
   module: {
     rules: [
       {
