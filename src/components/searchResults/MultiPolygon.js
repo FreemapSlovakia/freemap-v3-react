11f61d42a0079ce8406509b0fdb02a4528b94257
Martin Ždila <martin@gruveo.com>
2017-03-09 12:57:46 +0100

    improved formatting

--- src/components/searchResults/MultiPolygon.js
+++ src/components/searchResults/MultiPolygon.js
@@ -2,8 +2,9 @@ import React from 'react';
 import { Polygon as LeafletPolygon } from 'react-leaflet';
 
 export default function MultiPolygon({ searchResult, theme }) {
-  const polygonsLatLons = searchResult.geojson.coordinates
-    .map(polygonCoords => polygonCoords[0].map(lonlat => L.latLng(lonlat[1], lonlat[0])));
+  const polygonsLatLons = searchResult.geojson.coordinates.map(
+    polygonCoords => polygonCoords[0].map(lonlat => L.latLng(lonlat[1], lonlat[0]))
+  );
 
   const color = theme === 'selected' ? 'green' : 'grey';
   const leafletOptions = { fillColor: color, color };
