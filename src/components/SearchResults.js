c179ab49dc817b8fd940b13fc2faec6207a2ccaf
Peter Vojtek <peter.vojtek@gmail.com>
2017-03-09 14:51:22 +0100

    #35: search - add support for MultiLineString

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import Point from 'fm3/components/searchResults/Point';
 import Polyline from 'fm3/components/searchResults/Polyline';
 import Polygon from 'fm3/components/searchResults/Polygon';
+import MultiLineString from 'fm3/components/searchResults/MultiLineString';
 import MultiPolygon from 'fm3/components/searchResults/MultiPolygon';
 
 class SearchResults extends React.Component {
@@ -18,6 +19,9 @@ class SearchResults extends React.Component {
         {displayAsPolyline(highlightedResult) &&
           <Polyline searchResult={highlightedResult} theme="highlighted" />
         }
+        {displayAsMultiLineString(highlightedResult) &&
+          <MultiLineString searchResult={highlightedResult} theme="highlighted" />
+        }
         {displayAsPolygon(highlightedResult) &&
           <Polygon searchResult={highlightedResult} theme="highlighted" />
         }
@@ -29,6 +33,9 @@ class SearchResults extends React.Component {
         }
         {displayAsPolyline(selectedResult) &&
           <Polyline searchResult={selectedResult} theme="selected" />
+        }
+        {displayAsMultiLineString(selectedResult) &&
+          <MultiLineString searchResult={selectedResult} theme="selected" />
         }
         {displayAsPolygon(selectedResult) &&
           <Polygon searchResult={selectedResult} theme="selected" />
@@ -60,6 +67,10 @@ function displayAsPolygon(result) {
 
 function displayAsMultiPolygon(result) {
   return result && (result.geojson.type === 'MultiPolygon');
+}
+
+function displayAsMultiLineString(result) {
+  return result && (result.geojson.type === 'MultiLineString');
 }
 
 export default connect(
