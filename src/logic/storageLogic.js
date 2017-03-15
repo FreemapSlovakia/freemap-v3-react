5acb10c09ff1c703069da0b16fa4d8afca05ae0f
Peter Vojtek <peter.vojtek@gmail.com>
2017-03-15 19:29:41 +0100

    remove tool from persistent storage

--- src/logic/storageLogic.js
+++ src/logic/storageLogic.js
@@ -8,9 +8,6 @@ export default createLogic({
     const mapState = state.map;
 
     const appState = {
-      // main: {
-      //   tool: state.main.tool, // or maybe not?
-      // },
       routePlanner: {
         transportType: state.routePlanner.transportType
       },
