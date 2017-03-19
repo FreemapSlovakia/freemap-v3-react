753f055e7a6339bd767992e8486678cd54fbcec6
Peter Vojtek <peter.vojtek@gmail.com>
2017-03-19 18:41:42 +0100

    show diff in redux logger

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ import Main from 'fm3/components/Main';
 import 'fm3/styles/global.scss';
 
 const middleware = applyMiddleware(
-  createLogger(),
+  createLogger({ diff: true }),
   createLogicMiddleware(logics)
 );
 
