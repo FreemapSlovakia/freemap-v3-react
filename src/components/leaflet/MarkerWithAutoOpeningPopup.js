83a56a00e86d4007aadbedd6df0dae1bc906fab8
Martin Ždila <m.zdila@gmail.com>
2017-03-22 21:02:02 +0100

    fixed indentation

--- src/components/leaflet/MarkerWithAutoOpeningPopup.js
+++ src/components/leaflet/MarkerWithAutoOpeningPopup.js
@@ -1,13 +1,13 @@
 import { Marker } from 'react-leaflet';
 
 export default class MarkerWithAutoOpeningPopup extends Marker {
-    componentDidMount() {
-        super.componentDidMount();
-      this.leafletElement.openPopup();
-    }
+  componentDidMount() {
+    super.componentDidMount();
+    this.leafletElement.openPopup();
+  }
 
-    componentDidUpdate(prevProps, prevState) {
-        super.componentDidUpdate(prevProps, prevState);
-      this.leafletElement.openPopup();
-    }
+  componentDidUpdate(prevProps, prevState) {
+    super.componentDidUpdate(prevProps, prevState);
+    this.leafletElement.openPopup();
+  }
 }
