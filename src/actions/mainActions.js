fb030cd73789192d4c518e8f993b11b2df1d790f
Martin Ždila <martin@gruveo.com>
2017-03-22 10:21:38 +0100

    simplified mainActions

--- src/actions/mainActions.js
+++ src/actions/mainActions.js
@@ -1,11 +1,9 @@
 import { createActions } from 'redux-actions';
 
 const { setActivePopup, closePopup, setTool, setHomeLocation, startProgress, stopProgress } = createActions(
-  {
-    SET_ACTIVE_POPUP: activePopup => activePopup,
-    SET_TOOL: tool => tool,
-    SET_HOME_LOCATION: homeLocation => homeLocation
-  },
+  'SET_ACTIVE_POPUP',
+  'SET_TOOL',
+  'SET_HOME_LOCATION',
   'START_PROGRESS',
   'CLOSE_POPUP',
   'STOP_PROGRESS'
