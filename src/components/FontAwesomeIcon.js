7a67c4ff5e350b3728cae76ba4f7f60732e6a881
Peter Vojtek <peter.vojtek@gmail.com>
2017-03-24 11:48:36 +0100

    refactor: simplify FontAwesomeIcon.js component

--- src/components/FontAwesomeIcon.js
+++ src/components/FontAwesomeIcon.js
@@ -1,11 +1,8 @@
 import React from 'react';
-import 'fm3/styles/fontAwesomeIcon.scss';
 
 export default function FontAwesomeIcon({ icon, style }) {
   return (
-    <span className="fa-icon-container">
-      <i className={`fa fa-${icon}`} aria-hidden="true" style={style || {}}/>
-    </span>
+    <i className={`fa-fw fa fa-${icon}`} aria-hidden="true" style={style || {}}/>
   );
 }
 
