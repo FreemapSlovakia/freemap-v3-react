c46b3311e25e26cff0854e2104fb4ff32d6bcfea
Peter Vojtek <peter.vojtek@gmail.com>
2017-04-05 11:09:57 +0200

    #69: show track waypoints content via tooltip

--- src/components/TrackViewerResult.js
+++ src/components/TrackViewerResult.js
@@ -11,8 +11,19 @@ class TrackViewerResult extends React.Component {
   render() {
     const { trackGeojson } = this.props;
     const keyToAssureProperRefresh = Math.random(); // otherwise GeoJSON will still display the first data
+    const onEachFeature = (feature, layer) => {
+      // does this feature have a property named popupContent?
+      if (feature.geometry.type === 'Point' && feature.properties.name) {
+        layer.bindTooltip(feature.properties.name, { direction: 'right', className: 'compact' });
+      }
+    };
     return trackGeojson && (
-      <GeoJSON data={trackGeojson} key={keyToAssureProperRefresh} style={{ weight: 6, opacity: 0.85 }} />
+      <GeoJSON
+        data={trackGeojson}
+        key={keyToAssureProperRefresh}
+        onEachFeature={onEachFeature}
+        style={{ weight: 6, opacity: 0.85 }}
+      />
     );
   }
 }
