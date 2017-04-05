80b0b21f535eb4caf5d379870f447720f0f9654c
Peter Vojtek <peter.vojtek@gmail.com>
2017-04-05 18:01:02 +0200

    #69: show info (distance from start) on track mouse over

--- src/components/TrackViewerResult.js
+++ src/components/TrackViewerResult.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { GeoJSON, Tooltip } from 'react-leaflet';
-import * as FmPropTypes from 'fm3/propTypes';
 import MarkerWithInnerLabel from 'fm3/components/leaflet/MarkerWithInnerLabel';
 import strftime from 'strftime';
+import turfLineSlice from '@turf/line-slice';
+import turfLineDistance from '@turf/line-distance';
 
 class TrackViewerResult extends React.Component {
 
@@ -22,22 +23,55 @@ class TrackViewerResult extends React.Component {
       finishTime: React.PropTypes.string,
     })),
   }
+
+  state = {
+    infoLat: undefined,
+    infoLon: undefined,
+    infoDistanceKm: undefined,
+  }
+
+  // we keep here only business logic which needs access to the layer (otherwise use trackViewerLogic)
+  onEachFeature = (feature, layer) => {
+    if (feature.geometry.type === 'Point' && feature.properties.name) {
+      layer.bindTooltip(feature.properties.name, { direction: 'right', className: 'compact' });
+    }
+
+    if (feature.geometry.type === 'LineString') {
+      layer.on('mouseover', (e) => {
+        const infoLat = e.latlng.lat;
+        const infoLon = e.latlng.lng;
+        const infoDistanceKm = this.computeInfoDistanceKm(infoLat, infoLon, feature);
+        this.setState({ infoLat, infoLon, infoDistanceKm });
+      });
+      layer.on('mouseout', () => {
+        this.setState({ infoLat: undefined, infoLon: undefined, infoDistanceKm: undefined });
+      });
+    }
+  };
+
+  computeInfoDistanceKm = (infoLat, infoLon, geojsonLineString) => {
+    const p1 = {
+      type: 'Feature',
+      geometry: { type: 'Point', coordinates: geojsonLineString.geometry.coordinates[0] },
+    };
+    const p2 = {
+      type: 'Feature',
+      geometry: { type: 'Point', coordinates: [infoLon, infoLat] },
+    };
+    const s = turfLineSlice(p1, p2, geojsonLineString);
+    return turfLineDistance(s);
+  }
+
   render() {
     const { trackGeojson, startPoints, finishPoints } = this.props;
-    const keyToAssureProperRefresh = Math.random(); // otherwise GeoJSON will still display the first data
-    const onEachFeature = (feature, layer) => {
-      if (feature.geometry.type === 'Point' && feature.properties.name) {
-        // cannot move this into trackViewerLogic as we need to know the layer to bind to.
-        layer.bindTooltip(feature.properties.name, { direction: 'right', className: 'compact' });
-      }
-    };
+    const keyToAssureProperRefresh = JSON.stringify(trackGeojson).length; // otherwise GeoJSON will still display the first data
+
     return trackGeojson && (
       <div>
         <GeoJSON
           data={trackGeojson}
-          interactive={false}
           key={keyToAssureProperRefresh}
-          onEachFeature={onEachFeature}
+          onEachFeature={this.onEachFeature}
           style={{ weight: 6, opacity: 0.85 }}
         />
 
@@ -73,6 +107,21 @@ class TrackViewerResult extends React.Component {
             </Tooltip>
           </MarkerWithInnerLabel>
           ))}
+
+          {this.state.infoLat &&
+            <MarkerWithInnerLabel
+              faIcon="info"
+              faIconLeftPadding="2px"
+              color="grey"
+              interactive={false}
+              position={L.latLng(this.state.infoLat, this.state.infoLon)}
+            >
+              <Tooltip className="compact" offset={new L.Point(9, -25)} direction="right" permanent>
+                <span>
+                  {this.state.infoDistanceKm.toFixed(1)}km
+                </span>
+              </Tooltip>
+            </MarkerWithInnerLabel>}
       </div>
     );
   }
