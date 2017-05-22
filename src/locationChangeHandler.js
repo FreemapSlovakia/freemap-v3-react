19599861bc0a4fdf6c6d2e227f642826bcf700d7
Martin Ždila <martin@gruveo.com>
2017-05-22 10:32:23 +0200

    removed debug log

--- src/locationChangeHandler.js
+++ src/locationChangeHandler.js
@@ -10,7 +10,6 @@ export default function handleLocationChange(store, location) {
     const points = query.points.split(',').map(point => point.split('/').map(coord => parseFloat(coord)));
 
     if (points.length > 1 && points.every(point => point.length === 2) && /car|walk|bicycle/.test(query.transport)) {
-      console.log('XXXXXXXXX2');
       const latLons = points.map(([lat, lon]) => ({ lat, lon }));
       store.dispatch(routePlannerSetParams(latLons[0], latLons[latLons.length - 1], latLons.slice(1, latLons.length - 1), query.transport));
     }
