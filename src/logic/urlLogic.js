2eb8357d4f75e91f010656b5d52e9c72b16c79a7
Martin Ždila <m.zdila@gmail.com>
2017-06-08 20:54:38 +0200

    fixed reseting URL on logo click (fixes #133)

--- src/logic/urlLogic.js
+++ src/logic/urlLogic.js
@@ -2,7 +2,7 @@ import { createLogic } from 'redux-logic';
 import history from 'fm3/history';
 
 export const urlLogic = createLogic({
-  type: ['MAP_REFOCUS', /^ROUTE_PLANNER_/, 'SET_TOOL', 'SET_EMBEDDED_MODE'],
+  type: ['MAP_REFOCUS', /^ROUTE_PLANNER_/, 'SET_TOOL', 'SET_EMBEDDED_MODE', 'MAP_RESET'],
   process({ getState }, dispatch, done) {
     const {
       map: { mapType, overlays, zoom, lat, lon },
