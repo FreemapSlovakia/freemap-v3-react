ed3f9107b8e9a149a246c0beb1f5685471a4e09e
Peter Vojtek <peter.vojtek@gmail.com>
2017-06-21 11:06:25 +0200

    #143: allow info point position to be changed by dragging as well

--- src/components/InfoPoint.js
+++ src/components/InfoPoint.js
@@ -2,15 +2,17 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Tooltip } from 'react-leaflet';
 
+import { infoPointChangePosition } from 'fm3/actions/infoPointActions';
 import MarkerWithInnerLabel from 'fm3/components/leaflet/MarkerWithInnerLabel';
 import PropTypes from 'prop-types';
 
-function InfoPoint({ lat, lon, label }) {
+function InfoPoint({ lat, lon, label, inEditMode, onInfoPointChangePosition }) {
   return (
     lat && <MarkerWithInnerLabel
       faIcon="info"
       faIconLeftPadding="2px"
-      interactive={false}
+      draggable={inEditMode}
+      onDragend={e => onInfoPointChangePosition(e.target.getLatLng().lat, e.target.getLatLng().lng)}
       position={L.latLng(lat, lon)}
     >
       { label && <Tooltip className="compact" offset={new L.Point(9, -25)} direction="right" permanent>
@@ -26,6 +28,7 @@ InfoPoint.propTypes = {
   lat: PropTypes.number,
   lon: PropTypes.number,
   label: PropTypes.string,
+  inEditMode: PropTypes.bool.isRequired,
 };
 
 export default connect(
@@ -33,5 +36,12 @@ export default connect(
     lat: state.infoPoint.lat,
     lon: state.infoPoint.lon,
     label: state.infoPoint.label,
+    inEditMode: state.infoPoint.inEditMode,
+    onInfoPointChangePosition: PropTypes.func.isRequired,
+  }),
+  dispatch => ({
+    onInfoPointChangePosition(lat, lon) {
+      dispatch(infoPointChangePosition(lat, lon));
+    },
   }),
 )(InfoPoint);
