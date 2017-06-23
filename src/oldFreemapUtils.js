9ddc56d7c4df55b4d490d35b923bfb8fbc386cbc
Peter Vojtek <peter.vojtek@gmail.com>
2017-06-23 10:05:53 +0200

    #141: refactor oldFreemapUtils to DRY

--- src/oldFreemapUtils.js
+++ src/oldFreemapUtils.js
@@ -5,18 +5,12 @@ export function getTrasformedParamsIfIsOldFreemapUrl(location) {
   const isFromOldFreemapUrlFormat1 = location.hash && (location.hash.indexOf('#p=') === 0 || location.hash.indexOf('#m=') === 0); // #m=T,p=48.21836|17.4166|16|T
   const isFromOldFreemapUrlFormat2 = location.search && location.search.indexOf('m=') >= 0; // "?m=A&p=48.1855|17.4029|14"
   if (isFromOldFreemapUrlFormat1 || isFromOldFreemapUrlFormat2) {
-    const oldFreemapUrlParams = {};
-    let oldFreemapRawUrlParams;
+    let oldFreemapUrlParams;
     if (isFromOldFreemapUrlFormat1) {
-      oldFreemapRawUrlParams = location.hash.substring(1).split(','); // #m=T,p=48.21836|17.4166|16|T
+      oldFreemapUrlParams = rawUrlParamsToHash(location.hash, ','); // #m=T,p=48.21836|17.4166|16|T -> {'m': 'T', 'p' : '48.21836|17.4166|16|T'}
     } else { // isFromOldFreemapUrlFormat2
-      oldFreemapRawUrlParams = location.search.substring(1).split('&'); // ?m=A&p=48.1855|17.4029|14
+      oldFreemapUrlParams = rawUrlParamsToHash(location.search, '&'); // ?m=A&p=48.1855|17.4029|14
     }
-    oldFreemapRawUrlParams.forEach((s) => {
-      const [key, value] = s.split('=');
-      oldFreemapUrlParams[key] = value;
-    });  // {'m': 'T', 'p' : '48.21836|17.4166|16|T'}
-
     const [latFrag, lonFrag, zoomFrag, anotherMapTypeParam] = oldFreemapUrlParams.p.split('|');
     const mapType = oldFreemapUrlParams.m || anotherMapTypeParam || 'T';
     return {
@@ -31,14 +25,10 @@ export function getTrasformedParamsIfIsOldFreemapUrl(location) {
 }
 
 // http://embedded.freemap.sk/?lon=19.35&lat=48.55&zoom=8&marker=1&layers=A
+// http://embed2.freemap.sk/index.html?lat=48.79&lon=19.55&zoom=12&layers=T&markerLat=48.8&markerLon=19.6&markerHtml=Hello&markerShowPopup=1
 export function getTrasformedParamsIfIsOldEmbeddedFreemapUrl(location) {
-  if (window.location.origin.indexOf('embed.freemap.sk') >= 0 || location.search.indexOf('marker=1') >= 0) {
-    const oldFreemapRawUrlParams = location.search.substring(1).split('&');
-    const oldFreemapUrlParams = {};
-    oldFreemapRawUrlParams.forEach((s) => {
-      const [key, value] = s.split('=');
-      oldFreemapUrlParams[key] = value;
-    });
+  if (location.search.indexOf('marker=1') >= 0 || location.search.indexOf('markerLat') >= 0) {
+    const oldFreemapUrlParams = rawUrlParamsToHash(location.search, '&');
     const out = {
       lat: parseFloat(oldFreemapUrlParams.lat),
       lon: parseFloat(oldFreemapUrlParams.lon),
@@ -49,4 +39,15 @@ export function getTrasformedParamsIfIsOldEmbeddedFreemapUrl(location) {
     return out;
   }
   return false;
+}
+
+function rawUrlParamsToHash(rawParams, separator) {
+  const oldFreemapRawUrlParams = rawParams.substring(1).split(separator);
+  const oldFreemapUrlParams = {};
+  oldFreemapRawUrlParams.forEach((s) => {
+    const [key, value] = s.split('=');
+    oldFreemapUrlParams[key] = value;
+  });
+
+  return oldFreemapUrlParams;
 }
