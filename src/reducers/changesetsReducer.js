63eb780583755af625232bbf76cec63f49d254f5
Martin Ždila <m.zdila@gmail.com>
2017-06-29 00:26:33 +0200

    cleanup

--- src/reducers/changesetsReducer.js
+++ src/reducers/changesetsReducer.js
@@ -5,15 +5,12 @@ const initialState = {
 
 export default function changesets(state = initialState, action) {
   switch (action.type) {
-    case 'CHANGESETS_ADD': {
+    case 'CHANGESETS_ADD':
       return { ...state, changesets: action.payload.changesets };
-    }
-    case 'CHANGESETS_SET_DAYS': {
+    case 'CHANGESETS_SET_DAYS':
       return { ...state, days: action.payload.days };
-    }
     case 'SET_TOOL':
       return initialState;
-    case 'CHANGESETS_REFRESH':
     default:
       return state;
   }
