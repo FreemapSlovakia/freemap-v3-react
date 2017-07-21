2629486bf28c9bd64c6282aefa9dab0f5165fb67
Martin Ždila <m.zdila@gmail.com>
2017-07-22 00:12:01 +0200

    moved gallery viewer modal to separate file

--- src/components/GalleryResult.js
+++ src/components/GalleryResult.js
@@ -2,33 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Circle, TileLayer } from 'react-leaflet';
-import FontAwesomeIcon from 'fm3/components/FontAwesomeIcon';
-
-import Modal from 'react-bootstrap/lib/Modal';
-import Glyphicon from 'react-bootstrap/lib/Glyphicon';
-import Button from 'react-bootstrap/lib/Button';
-import Image from 'react-bootstrap/lib/Image';
 
 import mapEventEmitter from 'fm3/emitters/mapEventEmitter';
 
-import { galleryRequestImages, gallerySetImages, gallerySetActiveImageId, galleryShowOnTheMap }
-  from 'fm3/actions/galleryActions';
+import GalleryViewerModal from 'fm3/components/GalleryViewerModal';
+
+import { galleryRequestImages } from 'fm3/actions/galleryActions';
 
 import 'fm3/styles/gallery.scss';
-
-const dateFormat = new Intl.DateTimeFormat('sk');
 
 class GalleryResult extends React.Component {
   static propTypes = {
     onImageRequest: PropTypes.func.isRequired,
-    images: PropTypes.arrayOf(PropTypes.shape({
-      id: PropTypes.number.isRequired,
-    })).isRequired,
     activeImageId: PropTypes.number,
-    onClose: PropTypes.func.isRequired,
     zoom: PropTypes.number.isRequired,
-    onImageSelect: PropTypes.func.isRequired,
-    onShowOnTheMap: PropTypes.func.isRequired,
     pickingPosition: PropTypes.bool,
   }
 
@@ -46,61 +33,6 @@ class GalleryResult extends React.Component {
     mapEventEmitter.removeListener('mouseOut', this.handleMouseOut);
   }
 
-  // TODO separate to standalone component
-  getModal() {
-    const { images, activeImageId, onClose, onShowOnTheMap } = this.props;
-    const index = activeImageId ? images.findIndex(({ id }) => id === activeImageId) : -1;
-    const { path, title, description, author, createdAt } = images[index];
-
-    return (
-      <Modal show onHide={onClose} bsSize="large">
-        <Modal.Header closeButton>
-          <Modal.Title>
-            Obrázky {title && `:: ${title}`}
-          </Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <div className="carousel">
-            <div className="item active">
-              <a
-                href={`http://www.freemap.sk/upload/gallery/${path}`}
-                target="freemap_gallery_image"
-              >
-                <Image
-                  className="gallery-image"
-                  // src={`http://www.freemap.sk/lib/image.php?width=558&height=558&filename=upload/gallery/${path}`}
-                  src={`http://www.freemap.sk/upload/gallery/${path}`}
-                  alt={title}
-                />
-              </a>
-            </div>
-            <a
-              className={`left carousel-control ${index < 1 ? 'disabled' : ''}`}
-              onClick={this.handlePreviousClick}
-            >
-              <Glyphicon glyph="chevron-left" />
-            </a>
-            <a
-              className={`right carousel-control ${index >= images.length - 1 ? 'disabled' : ''}`}
-              onClick={this.handleNextClick}
-            >
-              <Glyphicon glyph="chevron-right" />
-            </a>
-          </div>
-          <p>
-            <br />
-            Nahral <b>{author}</b> dňa <b>{dateFormat.format(createdAt)}</b>
-            {description && `: ${description}`}
-          </p>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button onClick={onShowOnTheMap}><FontAwesomeIcon icon="dot-circle-o" /> Ukázať na mape</Button>
-          <Button onClick={onClose}><Glyphicon glyph="remove" /> Zavrieť</Button>
-        </Modal.Footer>
-      </Modal>
-    );
-  }
-
   handleMapClick = (lat, lon) => {
     if (this.props.pickingPosition) {
       // TODO
@@ -115,25 +47,6 @@ class GalleryResult extends React.Component {
 
   handleMouseOut = () => {
     this.setState({ lat: undefined, lon: undefined });
-  }
-
-  handlePreviousClick = (e) => {
-    e.preventDefault();
-
-    const { images, activeImageId, onImageSelect } = this.props;
-    const index = images.findIndex(({ id }) => id === activeImageId);
-    if (index > 0) {
-      onImageSelect(images[index - 1].id);
-    }
-  }
-
-  handleNextClick = (e) => {
-    e.preventDefault();
-    const { images, activeImageId, onImageSelect } = this.props;
-    const index = images.findIndex(({ id }) => id === activeImageId);
-    if (index + 1 < images.length) {
-      onImageSelect(images[index + 1].id);
-    }
   }
 
   render() {
@@ -159,7 +72,7 @@ class GalleryResult extends React.Component {
           />
         }
 
-        {activeImageId && this.getModal()}
+        {activeImageId && <GalleryViewerModal />}
       </div>
     );
   }
@@ -175,16 +88,6 @@ export default connect(
   dispatch => ({
     onImageRequest(lat, lon) {
       dispatch(galleryRequestImages(lat, lon));
-    },
-    onClose() {
-      dispatch(gallerySetImages([]));
-    },
-    onShowOnTheMap() {
-      dispatch(galleryShowOnTheMap());
-      dispatch(gallerySetImages([]));
-    },
-    onImageSelect(id) {
-      dispatch(gallerySetActiveImageId(id));
     },
   }),
 )(GalleryResult);
---
+++ src/components/GalleryViewerModal.js
@@ -0,0 +1,124 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import FontAwesomeIcon from 'fm3/components/FontAwesomeIcon';
+
+import Modal from 'react-bootstrap/lib/Modal';
+import Glyphicon from 'react-bootstrap/lib/Glyphicon';
+import Button from 'react-bootstrap/lib/Button';
+import Image from 'react-bootstrap/lib/Image';
+
+import mapEventEmitter from 'fm3/emitters/mapEventEmitter';
+
+import { gallerySetImages, gallerySetActiveImageId, galleryShowOnTheMap }
+  from 'fm3/actions/galleryActions';
+
+import 'fm3/styles/gallery.scss';
+
+const dateFormat = new Intl.DateTimeFormat('sk');
+
+class GalleryViewerModal extends React.Component {
+  static propTypes = {
+    images: PropTypes.arrayOf(PropTypes.shape({
+      id: PropTypes.number.isRequired,
+    })).isRequired,
+    activeImageId: PropTypes.number,
+    onClose: PropTypes.func.isRequired,
+    onImageSelect: PropTypes.func.isRequired,
+    onShowOnTheMap: PropTypes.func.isRequired,
+  }
+
+  handlePreviousClick = (e) => {
+    e.preventDefault();
+
+    const { images, activeImageId, onImageSelect } = this.props;
+    const index = images.findIndex(({ id }) => id === activeImageId);
+    if (index > 0) {
+      onImageSelect(images[index - 1].id);
+    }
+  }
+
+  handleNextClick = (e) => {
+    e.preventDefault();
+    const { images, activeImageId, onImageSelect } = this.props;
+    const index = images.findIndex(({ id }) => id === activeImageId);
+    if (index + 1 < images.length) {
+      onImageSelect(images[index + 1].id);
+    }
+  }
+
+  render() {
+    const { images, activeImageId, onClose, onShowOnTheMap } = this.props;
+    const index = activeImageId ? images.findIndex(({ id }) => id === activeImageId) : -1;
+    const { path, title, description, author, createdAt } = images[index];
+
+    return (
+      <Modal show onHide={onClose} bsSize="large">
+        <Modal.Header closeButton>
+          <Modal.Title>
+            Obrázky {title && `:: ${title}`}
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div className="carousel">
+            <div className="item active">
+              <a
+                href={`http://www.freemap.sk/upload/gallery/${path}`}
+                target="freemap_gallery_image"
+              >
+                <Image
+                  className="gallery-image"
+                  // src={`http://www.freemap.sk/lib/image.php?width=558&height=558&filename=upload/gallery/${path}`}
+                  src={`http://www.freemap.sk/upload/gallery/${path}`}
+                  alt={title}
+                />
+              </a>
+            </div>
+            <a
+              className={`left carousel-control ${index < 1 ? 'disabled' : ''}`}
+              onClick={this.handlePreviousClick}
+            >
+              <Glyphicon glyph="chevron-left" />
+            </a>
+            <a
+              className={`right carousel-control ${index >= images.length - 1 ? 'disabled' : ''}`}
+              onClick={this.handleNextClick}
+            >
+              <Glyphicon glyph="chevron-right" />
+            </a>
+          </div>
+          <p>
+            <br />
+            Nahral <b>{author}</b> dňa <b>{dateFormat.format(createdAt)}</b>
+            {description && `: ${description}`}
+          </p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={onShowOnTheMap}><FontAwesomeIcon icon="dot-circle-o" /> Ukázať na mape</Button>
+          <Button onClick={onClose}><Glyphicon glyph="remove" /> Zavrieť</Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  }
+}
+
+export default connect(
+  state => ({
+    images: state.gallery.images,
+    activeImageId: state.gallery.activeImageId,
+    zoom: state.map.zoom,
+    pickingPosition: state.gallery.pickingPositionForId !== null,
+  }),
+  dispatch => ({
+    onClose() {
+      dispatch(gallerySetImages([]));
+    },
+    onShowOnTheMap() {
+      dispatch(galleryShowOnTheMap());
+      dispatch(gallerySetImages([]));
+    },
+    onImageSelect(id) {
+      dispatch(gallerySetActiveImageId(id));
+    },
+  }),
+)(GalleryViewerModal);
