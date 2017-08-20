f451ad4d9922e1a491ad4fcb363beaccc0dbb392
Martin Ždila <m.zdila@gmail.com>
2017-08-20 23:06:42 +0200

    disabled buggy regexp cache for Intl

--- src/boot.js
+++ src/boot.js
@@ -1,9 +1,11 @@
+/* eslint-disable no-underscore-dangle */
 if (!global.Intl) {
   require.ensure([
     'intl',
     'intl/locale-data/jsonp/sk.js',
   ], (require) => {
     require('intl');
+    global.Intl.__disableRegExpRestore();
     require('intl/locale-data/jsonp/sk.js');
     require('.');
   });
