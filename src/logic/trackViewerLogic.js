2905c14ed1aa1c69ae2dda55bb6cf6eb4f4c47da
Martin Ždila <m.zdila@gmail.com>
2017-11-01 06:59:52 +0100

    fixed xpath evaluation for FF

--- src/logic/trackViewerLogic.js
+++ src/logic/trackViewerLogic.js
@@ -125,7 +125,7 @@ export const gpxLoadLogic = createLogic({
 
 function toNodes(data) {
   const nodes = {};
-  const nodeRes = data.evaluate('/osm/node', data, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE);
+  const nodeRes = data.evaluate('/osm/node', data, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null);
   for (let x = nodeRes.iterateNext(); x; x = nodeRes.iterateNext()) {
     nodes[x.getAttribute('id')] = [parseFloat(x.getAttribute('lon')), parseFloat(x.getAttribute('lat'))];
   }
@@ -134,11 +134,11 @@ function toNodes(data) {
 
 function toWays(data, nodes) {
   const ways = {};
-  const wayRes = data.evaluate('/osm/way', data, null, XPathResult.ORDERED_NODE_ITERATOR_TYPE);
+  const wayRes = data.evaluate('/osm/way', data, null, XPathResult.ORDERED_NODE_ITERATOR_TYPE, null);
   for (let x = wayRes.iterateNext(); x; x = wayRes.iterateNext()) {
     const coordinates = [];
 
-    const ndRefRes = data.evaluate('nd/@ref', x, null, XPathResult.ORDERED_NODE_ITERATOR_TYPE);
+    const ndRefRes = data.evaluate('nd/@ref', x, null, XPathResult.ORDERED_NODE_ITERATOR_TYPE, null);
     for (let y = ndRefRes.iterateNext(); y; y = ndRefRes.iterateNext()) {
       coordinates.push(nodes[y.value]);
     }
@@ -239,7 +239,7 @@ export const osmLoadRelationLogic = createLogic({
 
         const features = [];
 
-        const relationRes = data.evaluate('/osm/relation/member', data, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE);
+        const relationRes = data.evaluate('/osm/relation/member', data, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null);
         for (let x = relationRes.iterateNext(); x; x = relationRes.iterateNext()) {
           const type = x.getAttribute('type');
           const ref = x.getAttribute('ref');
