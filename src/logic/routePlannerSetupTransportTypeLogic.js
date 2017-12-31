118207115f60a10719afe27d80d6e29ba25d0e02
Martin Ždila <m.zdila@gmail.com>
2017-12-31 14:58:18 +0100

    fixed automatic transport type setting by map type

--- src/logic/routePlannerSetupTransportTypeLogic.js
+++ src/logic/routePlannerSetupTransportTypeLogic.js
@@ -6,11 +6,12 @@ import { routePlannerSetTransportType } from 'fm3/actions/routePlannerActions';
 export default createLogic({
   type: at.SET_TOOL,
   process({ getState }, dispatch, done) {
-    const { main: { tool }, routePlanner: { transportType, start, finish } } = getState();
-    if (tool === 'route-planner' && (!transportType) || !start && !finish) {
+    const { main: { tool }, routePlanner: { start, finish } } = getState();
+    if (tool === 'route-planner' && !(start && finish)) {
       const { mapType } = getState().map;
       dispatch(routePlannerSetTransportType(
-        mapType === 'T' ? 'foot' : mapType === 'K' ? 'nordic' : ['C', 'M'].includes(mapType) ? 'bike' : 'car'));
+        ['T', 'p'].includes(mapType) ? 'foot' : mapType === 'K' ? 'nordic'
+          : ['C', 'M'].includes(mapType) ? 'bike' : mapType === 'd' ? 'imhd' : 'car'));
     }
 
     done();
