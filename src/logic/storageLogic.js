5c3582ea85fd144a41114f7ff587b144d97812cd
Martin Ždila <martin@gruveo.com>
2018-01-03 09:03:33 +0100

    fixed automatic language persisting

--- src/logic/storageLogic.js
+++ src/logic/storageLogic.js
@@ -28,7 +28,7 @@ export default createLogic({
       trackViewer: {
         eleSmoothingFactor: state.trackViewer.eleSmoothingFactor,
       },
-      language: state.l10n.language || undefined,
+      language: state.l10n.language,
     };
 
     localStorage.setItem('appState', JSON.stringify(appState));
