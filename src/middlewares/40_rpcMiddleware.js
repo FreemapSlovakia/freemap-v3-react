3df0100f8142ae8b0ceb6689bf994bd61b423540
Martin Ždila <m.zdila@gmail.com>
2019-05-14 22:57:31 +0200

    implemented flushing rpc calls on ws closee

--- src/middlewares/40_rpcMiddleware.js
+++ src/middlewares/40_rpcMiddleware.js
@@ -1,13 +1,26 @@
 import * as at from 'fm3/actionTypes';
 import { wsSend, rpcResponse, rpcEvent } from 'fm3/actions/websocketActions';
 
+ // TODO implement timeout
+
 const callMap = new Map();
 let id = 0;
 
-export default ({ dispatch }) => next => (action) => {
+export default ({ dispatch, getState }) => next => (action) => {
+  const oldState = getState().websocket.state;
+
   next(action);
 
-  if (action.type === at.RPC_CALL) {
+  const { state } = getState().websocket;
+  if (oldState !== state) {
+    if (state !== 1) {
+      const values = callMap.values();
+      callMap.clear();
+      for (const call of values) {
+        dispatch(rpcResponse(call.method, call.params, undefined, { code: -31000, message: 'connection closed' }, call.tag));
+      }
+    }
+  } else if (action.type === at.RPC_CALL) {
     id += 1;
 
     callMap.put(id, {
