45e579a4e5ee10b4ad27e1f4894ec4ae3d940dc2
Martin Ždila <m.zdila@gmail.com>
2019-05-14 22:52:08 +0200

    added resubscribing

--- src/middlewares/trackingMiddleware.js
+++ src/middlewares/trackingMiddleware.js
@@ -6,17 +6,28 @@ export default ({ dispatch, getState }) => next => (action) => {
 
   next(action);
 
-  // TODO prevent concurrent subscribe/unsubscribe of the same id and keep their order
+  const { trackedDevices } = getState().tracking;
 
-  if (getState().tracking.trackedDevices.length === 0 && getState().websocket.state < 2) {
+  if (trackedDevices.length === 0 && getState().websocket.state < 2) {
     dispatch(wsClose());
-  } else if (getState().tracking.trackedDevices.length > 0 && getState().websocket.state === 3) {
+  } else if (trackedDevices.length > 0 && getState().websocket.state === 3) {
     dispatch(wsOpen());
-  } else if (prevState !== 1 && getState().websocket.state === 1 && getState().tracking.trackedDevices.length > 0) {
-    for (const td of getState().tracking.trackedDevices) {
+  } else if (prevState !== 1 && getState().websocket.state === 1 && trackedDevices.length > 0) {
+    for (const td of trackedDevices) {
       dispatch(rpcCall('tracking.subscribe', td));
     }
-  } else if (getState().websocket.state === 1 && prevTrackedDevices !== getState().tracking.trackedDevices) {
-    // TODO handle change with subscribe / unsubscribe (also "resubscribe")
+  } else if (getState().websocket.state === 1 && prevTrackedDevices !== trackedDevices) {
+    // TODO prevent concurrent subscribe/unsubscribe of the same id and keep their order
+    for (const td of prevTrackedDevices) {
+      if (!trackedDevices.includes(td)) {
+        const { token, deviceId } = td;
+        dispatch(rpcCall('tracking.unsubscribe', { token, deviceId }));
+      }
+    }
+    for (const td of trackedDevices) {
+      if (!prevTrackedDevices.includes(td)) {
+        dispatch(rpcCall('tracking.subscribe', td));
+      }
+    }
   }
 };
