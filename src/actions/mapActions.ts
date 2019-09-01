3b516007a879c767c05cc55972f112ee7b0c5de4
Martin Ždila <m.zdila@gmail.com>
2019-09-01 16:55:53 +0200

    cleanup

--- src/actions/mapActions.ts
+++ src/actions/mapActions.ts
@@ -2,20 +2,6 @@ import { createStandardAction, createAction } from 'typesafe-actions';
 
 export const mapReset = createAction('MAP_RESET');
 
-// TODO
-// let sn = []; // HACK safety net to prevent endless loop in case of some error
-
-// export function mapRefocus(changes) {
-//   const now = Date.now();
-//   sn = sn.filter(time => now - time < 1000);
-//   sn.push(now);
-//   if (sn.length > 10) {
-//     throw new Error('endless loop detected');
-//   }
-
-//   return { type: at.MAP_REFOCUS, payload: { ...changes } };
-// }
-
 export interface IMapViewState {
   mapType: string;
   lat: number;
