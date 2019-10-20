8d29b5b7e151bbd1cf61a93a2050f32a7cc97a6d
Martin Ždila <m.zdila@gmail.com>
2019-10-20 20:12:27 +0200

    fixed opening OSM login window on some browsers (fixes #470)

--- src/processors/authLoginWithOsmProcessor.ts
+++ src/processors/authLoginWithOsmProcessor.ts
@@ -4,30 +4,38 @@ import { httpRequest } from 'fm3/authAxios';
 import { assertType } from 'typescript-is';
 
 interface LoginResponse {
-  redirect?: string;
+  redirect: string;
 }
 
 export const authLoginWithOsmProcessor: Processor = {
   actionCreator: authLoginWithOsm,
   errorKey: 'logIn.logInError',
   handle: async ({ getState }) => {
-    const { data } = await httpRequest({
-      getState,
-      method: 'POST',
-      url: '/auth/login',
-      expectedStatus: 200,
-      cancelActions: [],
-    });
+    // open window within user gesture gandler (before await)
+    const w = window.open(
+      '',
+      'osm-login',
+      `width=600,height=550,left=${window.screen.width / 2 -
+        600 / 2},top=${window.screen.height / 2 - 550 / 2}`,
+    );
 
-    const { redirect } = assertType<LoginResponse>(data);
+    if (!w) {
+      throw new Error('window is null');
+    }
 
-    if (redirect) {
-      window.open(
-        redirect,
-        'osm-login',
-        `width=600,height=550,left=${window.screen.width / 2 -
-          600 / 2},top=${window.screen.height / 2 - 550 / 2}`,
-      );
+    try {
+      const { data } = await httpRequest({
+        getState,
+        method: 'POST',
+        url: '/auth/login',
+        expectedStatus: 200,
+        cancelActions: [],
+      });
+
+      w.location.href = assertType<LoginResponse>(data).redirect;
+    } catch (e) {
+      w.close();
+      throw e;
     }
   },
 };
