a19217cd4e8fda746a1d94e215779a5fb05dc0f3
Martin Ždila <m.zdila@gmail.com>
2019-12-07 14:41:26 +0100

    fixed duplicate keys

--- src/reducers/objectsReducer.ts
+++ src/reducers/objectsReducer.ts
@@ -15,10 +15,16 @@ export const objectsReducer = createReducer<ObjectsState, RootAction>(
   initialState,
 )
   .handleAction(clearMap, () => initialState)
-  .handleAction(objectsSetResult, (state, action) => ({
-    ...state,
-    objects: [...state.objects, ...action.payload],
-  }))
+  .handleAction(objectsSetResult, (state, action) => {
+    const newIds = new Set(action.payload.map(obj => obj.id));
+    return {
+      ...state,
+      objects: [
+        ...state.objects.filter(obj => !newIds.has(obj.id)),
+        ...action.payload,
+      ],
+    };
+  })
   .handleAction(deleteFeature, (state, action) => {
     const selection = action.meta?.selection;
     return selection?.type === 'objects'
