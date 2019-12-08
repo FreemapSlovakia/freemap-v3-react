7dd0695a373ed4d6b151d3ac64e384196a402da7
Martin Ždila <m.zdila@gmail.com>
2019-12-08 22:41:30 +0100

    ..

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -295,7 +295,7 @@ const MainInt: React.FC<Props> = ({
               {tool === 'gallery' && <GalleryMenu />}
               {tool === 'map-details' && <MapDetailsMenu />}
               {tool === 'tracking' && <TrackingMenu />}{' '}
-              {(((selection as any)?.id ?? null) || // TODO ugly detection if something is selected
+              {(((selection as any)?.id ?? null) !== null || // TODO ugly detection if something is selected
                 ((selection as any)?.index ?? null) !== null) && (
                 <Button title={t('general.delete')} onClick={onDelete}>
                   <FontAwesomeIcon icon="trash" />
