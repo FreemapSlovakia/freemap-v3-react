b2557c93bd25aadfe51c2575bb271b4dc7cc8591
Martin Ždila <m.zdila@gmail.com>
2020-12-15 10:22:29 +0100

    improved locate-me indicator

--- src/components/LocationResult.tsx
+++ src/components/LocationResult.tsx
@@ -1,8 +1,19 @@
-import { RichMarker } from 'fm3/components/RichMarker';
 import { RootState } from 'fm3/storeCreator';
+import { divIcon } from 'leaflet';
 import { ReactElement } from 'react';
-import { Circle } from 'react-leaflet';
+import { Circle, Marker } from 'react-leaflet';
 import { useSelector } from 'react-redux';
+
+const circularIcon = divIcon({
+  iconSize: [40, 40],
+  iconAnchor: [20, 20],
+  html: `
+  <svg width="40" height="40" viewBox="-20 -20 40 40">
+    <path d="M-5 0H-13v0M5 0h8v0M0-5v-8M0 5v8" stroke="#fff" stroke-width="3" stroke-linecap="round" stroke-opacity=".666"/>
+    <path d="M-5 0H-13v0" stroke="#000" stroke-width="1"/>
+    <path d="M5 0h8v0M0-5v-8M0 5v8" stroke="#000"/>
+  </svg>`,
+});
 
 export function LocationResult(): ReactElement | null {
   const gpsLocation = useSelector((state: RootState) => state.main.location);
@@ -13,10 +24,11 @@ export function LocationResult(): ReactElement | null {
         center={{ lat: gpsLocation.lat, lng: gpsLocation.lon }}
         radius={gpsLocation.accuracy / 2}
         weight={1}
+        stroke={false}
       />
-      <RichMarker
+      <Marker
+        icon={circularIcon}
         position={{ lat: gpsLocation.lat, lng: gpsLocation.lon }}
-        interactive={false}
       />
     </>
   );
