793980447413d637888b6144cb7fe4af2b0bea74
Martin Ždila <m.zdila@gmail.com>
2021-04-05 10:05:12 +0200

    fixed decoding gpx data

--- src/processors/trackViewerDownloadTrackProcessor.ts
+++ src/processors/trackViewerDownloadTrackProcessor.ts
@@ -20,7 +20,9 @@ export const trackViewerDownloadTrackProcessor: Processor = {
 
     dispatch(
       trackViewerSetData({
-        trackGpx: atob(assertType<{ data: string }>(data).data),
+        trackGpx: decodeURIComponent(
+          escape(atob(assertType<{ data: string }>(data).data)),
+        ),
       }),
     );
   },
