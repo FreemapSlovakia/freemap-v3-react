59ce3c7f430fd5f682d01ada600cd058cfee21d3
Martin Ždila <m.zdila@gmail.com>
2021-07-03 09:50:28 +0200

    made position marker even more prominent (refs #600)

--- src/components/LocationResult.tsx
+++ src/components/LocationResult.tsx
@@ -7,11 +7,11 @@ const circularIcon = divIcon({
   iconSize: [40, 40],
   iconAnchor: [20, 20],
   html: `<svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="-20 -20 40 40">
-  <g opacity=".67" fill="none" stroke="#fff" stroke-width="4" stroke-linecap="round">
+  <g opacity=".67" fill="none" stroke="#fff" stroke-width="4.5" stroke-linecap="round">
     <path d="M-8 .5h-8.5v0h-.5M9 .5h9M.5-8v-9M.5 9v9"/>
     <circle cx=".5" cy=".5" r="13" stroke-linejoin="round"/>
   </g>
-  <g fill="none" stroke="#000" stroke-linecap="round">
+  <g fill="none" stroke="#000" stroke-width="1.5" stroke-linecap="round">
     <path d="M-8 .5h-8.5v0h-.5M9 .5h9M.5-8v-9M.5 9v9"/>
     <circle cx=".5" cy=".5" r="13" stroke-linejoin="round"/>
   </g>
