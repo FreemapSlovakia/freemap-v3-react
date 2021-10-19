7fd511528b3a2fedd3ba82b840217e585da1e5ca
Martin Ždila <m.zdila@gmail.com>
2021-10-19 09:23:58 +0200

    fixed async component

--- src/components/AsyncComponent.tsx
+++ src/components/AsyncComponent.tsx
@@ -1,12 +1,12 @@
 import { useLazy } from 'fm3/hooks/useLazy';
-import { ComponentType, ReactElement, useMemo } from 'react';
+import { ComponentType, ReactElement } from 'react';
 
 type Props<T extends ComponentType<any> = ComponentType<any>> = {
   factory(): Promise<{ default: T }>;
 };
 
 export function AsyncComponent({ factory }: Props): ReactElement | null {
-  const Component = useLazy(useMemo(() => factory, [factory]));
+  const Component = useLazy(factory);
 
   return Component ? <Component /> : null;
 }
