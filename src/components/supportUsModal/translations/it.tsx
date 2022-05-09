899ca3cab8563b05ed5012772793b48ac19f8cf5
Martin Ždila <m.zdila@gmail.com>
2022-05-09 18:38:13 +0200

    formatting of IT translation file for supportUs

--- src/components/supportUsModal/translations/it.tsx
+++ src/components/supportUsModal/translations/it.tsx
@@ -5,7 +5,7 @@ const it: SupportUsMessages = {
     'Il portale di mappe Freemap è creato da volontari che lavorano gratuitamente nel loro tempo libero. Tuttavia per il suo funzionamento ha bisogno di hardware e di servizi di società commerciali che hanno un costo.',
   account: 'Conto bancario:',
   paypal: 'Fai una donazione con PayPal',
-  thanks: "Apprezzeremo ogni donazione. Grazie !",
+  thanks: 'Apprezzeremo ogni donazione. Grazie !',
   registration: 'Registrato nel MV/VVS/1-900/90-34343 il 2 Ottobre 2009',
 };
 
