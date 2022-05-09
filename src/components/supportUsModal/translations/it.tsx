d391c23ae4afbe9bf98a301d43c245439d3b1af3
dp7x <103643938+dp7x@users.noreply.github.com>
2022-05-09 17:22:16 +0200

    Create it.tsx
    I changed also the const EN to IT (lines 3, 12)

---
+++ src/components/supportUsModal/translations/it.tsx
@@ -0,0 +1,12 @@
+import { SupportUsMessages } from './SupportUsMessages';
+
+const it: SupportUsMessages = {
+  explanation:
+    'Il portale di mappe Freemap è creato da volontari che lavorano gratuitamente nel loro tempo libero. Tuttavia per il suo funzionamento ha bisogno di hardware e di servizi di società commerciali che hanno un costo.',
+  account: 'Conto bancario:',
+  paypal: 'Fai una donazione con PayPal',
+  thanks: "Apprezzeremo ogni donazione. Grazie !",
+  registration: 'Registrato nel MV/VVS/1-900/90-34343 il 2 Ottobre 2009',
+};
+
+export default it;
