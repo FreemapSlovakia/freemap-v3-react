02ee97341d9ca8dd3add1e1a299f44ffe4a19934
Martin Ždila <m.zdila@gmail.com>
2026-01-08 09:16:20 +0100

    fixed josm remote control

--- src/sw/sw.ts
+++ src/sw/sw.ts
@@ -24,6 +24,18 @@ self.addEventListener('install', (event) => {
 });
 
 self.addEventListener('fetch', (event) => {
+  const url = new URL(event.request.url);
+
+  if (
+    url.protocol === 'http:' &&
+    (url.hostname === 'localhost' ||
+      url.hostname === '127.0.0.1' ||
+      url.hostname === '::1') &&
+    url.port === '8111'
+  ) {
+    return;
+  }
+
   event.respondWith(
     (async () => {
       const cacheMode = (await get('cacheMode')) as undefined | CacheMode;
