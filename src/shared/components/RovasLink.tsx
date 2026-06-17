644fdb0dc6cba20e8ecc7cc477bb340fdaeb5001
Alexander Zatko <alexander.zatko@nabezky.sk>
2026-06-17 12:56:20 +0200

    removed link icon from the Chrons payment text

--- src/shared/components/RovasLink.tsx
+++ src/shared/components/RovasLink.tsx
@@ -1,5 +1,4 @@
 import type { ReactElement, ReactNode } from 'react';
-import { FaExternalLinkAlt } from 'react-icons/fa';
 
 type Props = {
   children: ReactNode;
@@ -8,7 +7,7 @@ type Props = {
 export function RovasLink({ children }: Props): ReactElement {
   return (
     <a href="https://rovas.app" target="_blank" rel="noopener noreferrer">
-      {children} <FaExternalLinkAlt />
+      {children}
     </a>
   );
 }
